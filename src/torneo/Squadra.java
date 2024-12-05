f81c80f8348f7cb6acc538d1babfe7590676637f
samuelepierotti <samy.pierotti@gmail.com>
2024-12-05 07:47:24 +0100

    Primo commit

---
+++ src/torneo/Squadra.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package torneo;
+
+/**
+ *
+ * @author B045_05
+ */
+public class Squadra {
+    
+}
