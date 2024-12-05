688f102bd6c899045762df0571b0fb6917d63ea8
samuelepierotti <samy.pierotti@gmail.com>
2024-12-05 08:07:04 +0100

    Modifica classe Arbitro

--- src/torneo/Arbitro.java
+++ src/torneo/Arbitro.java
@@ -1,3 +1,4 @@
+// Messaggio di test
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
