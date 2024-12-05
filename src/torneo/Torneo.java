f5dc0e4c6801fbe8cc84fe26dd700224ef2451ae
samuelepierotti <samy.pierotti@gmail.com>
2024-12-05 08:01:09 +0100

    Modifica classe di avvio Torneo

--- src/torneo/Torneo.java
+++ src/torneo/Torneo.java
@@ -1,3 +1,4 @@
+// Messaggio di test
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
